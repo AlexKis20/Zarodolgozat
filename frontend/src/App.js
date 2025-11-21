ed082cd5e1bfdc0b8ecfef444d09398ea9960ac2
AlexKis20 <kisrondox5@gmail.com>
2025-11-21 11:18:24 +0100

    fronted

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="container mt-3">
+        <Routes>
+          
+         
+          
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
