dd0101188afe37810d187af03c61949854e65fda
AlexKis20 <kisrondox5@gmail.com>
2026-02-24 08:59:11 +0100

    cim

--- frontend/src/Cim.js
+++ frontend/src/Cim.js
@@ -1,6 +1,6 @@
 module.exports={
-    Cim:"http://localhost:3000"
-    //Cim:"https://nodejs205.dszcbaross.edu.hu"
+    //Cim:"http://localhost:3000"
+    Cim:"https://nodejs205.dszcbaross.edu.hu"
 }
 
 
